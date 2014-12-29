54d2c80158f7f99774d19c3e09de5682fc0a88d5
yroffin@gmail.com <yroffin@gmail.com>
2014-12-29 01:36:16 +0100

    Validation beta

--- ille-et-zick/public_html/js/controllers.js
+++ ille-et-zick/public_html/js/controllers.js
@@ -25,16 +25,6 @@ angular.module('illeEtZick.controllers', [])
      */
     $scope.load = function() {
         $scope.theme="b";
-        $scope.inventoryItem = {href:''};
-        console.log("Template loading ...");
-        gasPublicServices.getInfo({message: {}},
-        function(data) {
-            console.log(data);
-            $scope.auth = data;
-            console.log("getInfo loaded ...");
-        },function(failure) {
-            $window.location.href = failure.config.url;
-        });
         
         // static header init
         $(function(){
@@ -48,47 +38,6 @@ angular.module('illeEtZick.controllers', [])
      */
     $scope.redirect = function(url) {
         $window.location.href = url;
-    }
-    $scope.loadInventory = function() {
-        console.log("getAllInventory loading ...");
-        gasPublicServices.getAllInventory({message: {}},
-        function(data) {
-            console.log(data);
-            $scope.inventory = data;
-            console.log("getAllInventory loaded ...");
-            setTimeout(function(){
-                $("#inventoryList").listview("refresh");
-            },100);
-        },function(failure) {
-            $window.location.href = failure.config.url;
-        });
-    }
-    $scope.loadGroups = function() {
-        console.log("findAllGroups loading ...");
-        gasPublicServices.findAllGroups({message: {key:'iez'}},
-        function(data) {
-            console.log(data);
-            $scope.groups = data;
-            console.log("findAllGroups loaded ...");
-            setTimeout(function(){
-                $("#groupsList").listview("refresh");
-            },100);
-        },function(failure) {
-            $window.location.href = failure.config.url;
-        });
-    }
-    $scope.selectItem = function(item) {
-        console.log("selectItem");
-        $scope.inventoryItem = item;
-        console.log(item);
-    }
-    $scope.selectGroup = function(item) {
-        console.log("selectGroup");
-        $scope.groupItem = item;
-        setTimeout(function(){
-            $("#groupsDetailList").listview("refresh");
-        },100);
-        console.log(item);
     }
 }])
 ;
